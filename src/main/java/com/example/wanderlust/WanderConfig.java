8e6dfe48685f3fef694f37cb3c915a65e5d75cbf
mddesignstl <mddesignstl@gmail.com>
2018-07-18 18:20:33 -0500

    fix the thing that was probably never broken

--- src/main/java/com/example/wanderlust/WanderConfig.java
+++ src/main/java/com/example/wanderlust/WanderConfig.java
@@ -1,5 +1,10 @@
 package com.example.wanderlust;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
 import javax.validation.constraints.NotNull;
 
 @Component
@@ -8,14 +13,15 @@ import javax.validation.constraints.NotNull;
 public class WanderConfig {
 
     @NotNull
-    private String googleApiKey = "AIzaSyAKuho6SiD7I-hkOQNpoWa9j0NntW4Wdz8";
+    private String googleApiKey;
 
-    public void setGoogleApiKey(String googleApiKey) {
-        this.googleApiKey = googleApiKey;
-    }
 
     public String getGoogleApiKey() {
         return this.googleApiKey;
+    }
+
+    public void setGoogleApiKey(String googleApiKey) {
+        this.googleApiKey = googleApiKey;
     }
 
 }
