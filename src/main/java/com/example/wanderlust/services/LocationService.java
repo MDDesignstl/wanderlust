e284bdf7f1d7e6eef9ae77be2317b7f5d4c8c8a7
mddesignstl <mddesignstl@gmail.com>
2018-07-18 12:59:25 -0500

    add autowire to wanderconfig's

--- src/main/java/com/example/wanderlust/services/LocationService.java
+++ src/main/java/com/example/wanderlust/services/LocationService.java
@@ -10,6 +10,7 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class LocationService {
 
+    @Autowired
     private static WanderConfig wanderConfig;
 
     @Autowired
--- src/main/java/com/example/wanderlust/services/SearchService.java
+++ src/main/java/com/example/wanderlust/services/SearchService.java
@@ -14,6 +14,7 @@ import java.util.*;
 @Service
 public class SearchService {
 
+    @Autowired
     private static WanderConfig wanderConfig;
 
     @Autowired
