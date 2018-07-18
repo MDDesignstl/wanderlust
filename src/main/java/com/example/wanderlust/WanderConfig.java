8a9681618b3b7815d298ec34443ade32ed56aabd
mddesignstl <mddesignstl@gmail.com>
2018-07-18 06:31:40 -0500

    hard code api key

--- src/main/java/com/example/wanderlust/WanderConfig.java
+++ src/main/java/com/example/wanderlust/WanderConfig.java
@@ -1,27 +1,16 @@
 package com.example.wanderlust;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.stereotype.Component;
-
 import javax.validation.constraints.NotNull;
 
-@Component
-@PropertySource("classpath:application.properties")
-@ConfigurationProperties
+
 public class WanderConfig {
 
     @NotNull
-    private String googleApiKey;
+    private String googleApiKey = "AIzaSyAKuho6SiD7I-hkOQNpoWa9j0NntW4Wdz8";
 
 
     public String getGoogleApiKey() {
         return this.googleApiKey;
-    }
-
-    public void setGoogleApiKey(String googleApiKey) {
-        this.googleApiKey = googleApiKey;
     }
 
 }
